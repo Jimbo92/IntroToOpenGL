f0fbd8ed7863f3acaab1fcbfffc833b9ca4528dc
James Stone <james.stone@ad.aie.edu>
2016-02-01 13:58:25 +1100

    Added gizmos

--- GLApp/GLApp/main.cpp
+++ GLApp/GLApp/main.cpp
@@ -1,5 +1,13 @@
 #include <gl_core_4_4.h>
 #include <GLFW\glfw3.h>
+#include <Gizmos.h>
+#include <glm\glm.hpp>
+#include <glm\ext.hpp>
+#include <iostream>
+
+using glm::vec3;
+using glm::vec4;
+using glm::mat4;
 
 int main()
 {
@@ -15,7 +23,9 @@ int main()
 		glfwTerminate();
 		return -2;
 	}
-	
+
+	glfwMakeContextCurrent(window);
+
 	if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
 	{
 		glfwDestroyWindow(window);
@@ -23,11 +33,46 @@ int main()
 		return -3;
 	}
 
-	glfwMakeContextCurrent(window);
+	Gizmos::create();
+
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
+
+	auto major = ogl_GetMajorVersion();
+	auto minor = ogl_GetMinorVersion();
+	printf("GL: %i.%i\n", major, minor);
+
+	while (glfwWindowShouldClose(window) == false && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
+	{
+		//clear buffer and set buffer color
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(1.f, 0.5f, 0.5f, 1.f);
+		glEnable(GL_DEPTH_TEST);
+
+		Gizmos::clear();
+
+		Gizmos::addTransform(mat4(1));
+
+		vec4 white(1);
+		vec4 black(0, 0, 0, 1);
+
+		for (int i = 0; i < 50; ++i)
+		{
+			Gizmos::addLine(vec3(-10 + i, 0, 10), vec3(-10 + i, 0, -10), i == 10 ? white : black);
+
+			Gizmos::addLine(vec3(10, 0, -10 + i), vec3(-10, 0, -10 + i), i == 10 ? white : black);
+		}
+
+		Gizmos::draw(projection * view);
+
+		//game logic
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 
-	//Code
-
+	Gizmos::destroy();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
